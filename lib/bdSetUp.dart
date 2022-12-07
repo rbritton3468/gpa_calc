554cc7772912373d619768e9694473fd1faad5b0
rbritton3468 <44485771+rbritton3468@users.noreply.github.com>
2022-12-07 11:56:07 -0600

    DB

--- lib/bdSetUp.dart
+++ lib/bdSetUp.dart
@@ -1,25 +1,34 @@
 import 'dart:async';
+
+import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() async {
+  // Avoid errors caused by flutter upgrade.
+  // Importing 'package:flutter/widgets.dart' is required.
+  WidgetsFlutterBinding.ensureInitialized();
+  // Open the database and store the reference.
+
   final database = openDatabase(
     // Set the path to the database. Note: Using the `join` function from the
     // `path` package is best practice to ensure the path is correctly
     // constructed for each platform.
-    join(await getDatabasesPath(), 'year.db'),
+    join(await getDatabasesPath(), 'doggie_database.db'),
     // When the database is first created, create a table to store dogs.
     onCreate: (db, version) {
       // Run the CREATE TABLE statement on the database.
       return db.execute(
-        'CREATE TABLE Year(id INTEGER PRIMARY KEY,cl1 REAl, cln1 INTEGER,cl2 REAl, cln2 INTEGER,cl3 REAl, cln3 INTEGER,cl4 REAl, cln4 INTEGER,cl5 REAl, cln5 INTEGER,cl6 REAl, cln6 INTEGER,cl7 REAl, cln7 INTEGER)',
+        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
       );
     },
     // Set the version. This executes the onCreate function and provides a
     // path to perform database upgrades and downgrades.
     version: 1,
   );
-  Future<void> insertYear(Year year) async {
+
+  // Define a function that inserts dogs into the database
+  Future<void> insertDog(Dog dog) async {
     // Get a reference to the database.
     final db = await database;
 
@@ -28,84 +37,114 @@ void main() async {
     //
     // In this case, replace any previous data.
     await db.insert(
-      'Year',
-      year.toMap(),
+      'dogs',
+      dog.toMap(),
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  var y1 = const Year(
-      id: 1,
-      cl1: 2,
-      cln1: 4,
-      cl2: 6,
-      cln2: 7,
-      cl3: 8,
-      cln3: 8,
-      cl4: 8,
-      cln4: 9,
-      cl5: 6,
-      cln5: 6,
-      cl6: 4,
-      cln6: 3,
-      cl7: 4,
-      cln7: 3);
+  // A method that retrieves all the dogs from the dogs table.
+  Future<List<Dog>> dogs() async {
+    // Get a reference to the database.
+    final db = await database;
 
-  await insertYear(y1);
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('dogs');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return Dog(
+        id: maps[i]['id'],
+        name: maps[i]['name'],
+        age: maps[i]['age'],
+      );
+    });
+  }
+
+  Future<void> updateDog(Dog dog) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Update the given Dog.
+    await db.update(
+      'dogs',
+      dog.toMap(),
+      // Ensure that the Dog has a matching id.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [dog.id],
+    );
+  }
+
+  Future<void> deleteDog(int id) async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Remove the Dog from the database.
+    await db.delete(
+      'dogs',
+      // Use a `where` clause to delete a specific dog.
+      where: 'id = ?',
+      // Pass the Dog's id as a whereArg to prevent SQL injection.
+      whereArgs: [id],
+    );
+  }
+
+  // Create a Dog and add it to the dogs table
+  var fido = const Dog(
+    id: 0,
+    name: 'Fido',
+    age: 35,
+  );
+
+  await insertDog(fido);
+
+  // Now, use the method above to retrieve all the dogs.
+  print(await dogs()); // Prints a list that include Fido.
+
+  // Update Fido's age and save it to the database.
+  fido = Dog(
+    id: fido.id,
+    name: fido.name,
+    age: fido.age + 7,
+  );
+  await updateDog(fido);
+
+  // Print the updated results.
+  print(await dogs()); // Prints Fido with age 42.
+
+  // Delete Fido from the database.
+  await deleteDog(fido.id);
+
+  // Print the list of dogs (empty).
+  print(await dogs());
 }
 
-class Year {
+class Dog {
   final int id;
-  final int cl1;
-  final int cln1;
-  final int cl2;
-  final int cln2;
-  final int cl3;
-  final int cln3;
-  final int cl4;
-  final int cln4;
-  final int cl5;
-  final int cln5;
-  final int cl6;
-  final int cln6;
-  final int cl7;
-  final int cln7;
+  final String name;
+  final int age;
 
-  const Year({
+  const Dog({
     required this.id,
-    required this.cl1,
-    required this.cln1,
-    required this.cl2,
-    required this.cln2,
-    required this.cl3,
-    required this.cln3,
-    required this.cl4,
-    required this.cln4,
-    required this.cl5,
-    required this.cln5,
-    required this.cl6,
-    required this.cln6,
-    required this.cl7,
-    required this.cln7,
+    required this.name,
+    required this.age,
   });
 
+  // Convert a Dog into a Map. The keys must correspond to the names of the
+  // columns in the database.
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'cl1': cl1,
-      'cln1': cln1,
-      'cl2': cl2,
-      'cln2': cln2,
-      'cl3': cl3,
-      'cln3': cln3,
-      'cl4': cl4,
-      'cln4': cln4,
-      'cl5': cl5,
-      'cln5': cln5,
-      'cl6': cl6,
-      'cln6': cln6,
-      'cl7': cl7,
-      'cln7': cln7,
+      'name': name,
+      'age': age,
     };
+  }
+
+  // Implement toString to make it easier to see information about
+  // each dog when using the print statement.
+  @override
+  String toString() {
+    return 'Dog{id: $id, name: $name, age: $age}';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,128 +10,54 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 void main() async {
-  runApp(const MyApp());
-
   WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
+Future<void> insertYear(Year year) async {
   final database = openDatabase(
-    'year.db',
-    onCreate: (db, version) {
-      // Run the CREATE TABLE statement on the database.
-      return db.execute(
-        'CREATE TABLE Year(id INTEGER PRIMARY KEY,cl1 REAl, cln1 INTEGER,cl2 REAl, cln2 INTEGER,cl3 REAl, cln3 INTEGER,cl4 REAl, cln4 INTEGER,cl5 REAl, cln5 INTEGER,cl6 REAl, cln6 INTEGER,cl7 REAl, cln7 INTEGER)',
-      );
-    },
+    join(await getDatabasesPath(), 'gradeBook.db'),
     // Set the version. This executes the onCreate function and provides a
     // path to perform database upgrades and downgrades.
     version: 1,
   );
-  Future<void> insertYear(Year year) async {
-    // Get a reference to the database.
-    final db = await database;
 
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
-    await db.insert(
-      'Year',
-      year.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
-    );
-  }
-
-/*Future<List<Year>> year() async {
   // Get a reference to the database.
   final db = await database;
 
-  // Query the table for all The Dogs.
-  final List<Map<String, dynamic>> maps = await db.query('Year');
-
-  // Convert the List<Map<String, dynamic> into a List<Dog>.
-  return List.generate(maps.length, (i) {
-    return Year(
-      id: maps[i]['id'],
-      cl1: maps[i]['cl1'],
-      cl2: ,
-      cl3: ,
-
-    );
-  });
-}*/
-
-  var y1 = const Year(
-      id: 1,
-      cl1: 2,
-      cln1: 4,
-      cl2: 6,
-      cln2: 7,
-      cl3: 8,
-      cln3: 8,
-      cl4: 8,
-      cln4: 9,
-      cl5: 6,
-      cln5: 6,
-      cl6: 4,
-      cln6: 3,
-      cl7: 4,
-      cln7: 3);
-
-  await insertYear(y1);
+  // Insert the Dog into the correct table. You might also specify the
+  // `conflictAlgorithm` to use in case the same dog is inserted twice.
+  //
+  // In this case, replace any previous data.
+  await db.insert(
+    'book',
+    year.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
 }
 
 class Year {
   final int id;
-  final int cl1;
-  final int cln1;
-  final int cl2;
-  final int cln2;
-  final int cl3;
-  final int cln3;
-  final int cl4;
-  final int cln4;
-  final int cl5;
-  final int cln5;
-  final int cl6;
-  final int cln6;
-  final int cl7;
-  final int cln7;
+  final int year;
+  final int classType;
+  final int classNum;
+  final double grade;
 
   const Year({
     required this.id,
-    required this.cl1,
-    required this.cln1,
-    required this.cl2,
-    required this.cln2,
-    required this.cl3,
-    required this.cln3,
-    required this.cl4,
-    required this.cln4,
-    required this.cl5,
-    required this.cln5,
-    required this.cl6,
-    required this.cln6,
-    required this.cl7,
-    required this.cln7,
+    required this.year,
+    required this.classType,
+    required this.classNum,
+    required this.grade,
   });
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
-      'cl1': cl1,
-      'cln1': cln1,
-      'cl2': cl2,
-      'cln2': cln2,
-      'cl3': cl3,
-      'cln3': cln3,
-      'cl4': cl4,
-      'cln4': cln4,
-      'cl5': cl5,
-      'cln5': cln5,
-      'cl6': cl6,
-      'cln6': cln6,
-      'cl7': cl7,
-      'cln7': cln7,
+      'year': year,
+      'classType': classType,
+      'classNum': classNum,
+      'grade': grade,
     };
   }
 }
@@ -237,7 +163,44 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
+Future<Database> opendb() async {
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'gradeBook.db'),
+    onCreate: (db, version) {
+      // Run the CREATE TABLE statement on the database.
+      return db.execute(
+        'CREATE TABLE book (id INTEGER PRIMARY KEY, year INTEGER, classType INTEGER, classNum INTEGER, grade REAL',
+      );
+    },
+
+    // Set the version. This executes the onCreate function and provides a
+    // path to perform database upgrades and downgrades.
+    version: 1,
+  );
+  return database;
+}
+
 class Calc extends StatelessWidget {
+  final database = opendb();
+  Future<List<Year>> year() async {
+    // Get a reference to the database.
+    final db = await database;
+
+    // Query the table for all The Dogs.
+    final List<Map<String, dynamic>> maps = await db.query('book');
+
+    // Convert the List<Map<String, dynamic> into a List<Dog>.
+    return List.generate(maps.length, (i) {
+      return Year(
+        id: maps[i]['id'],
+        year: maps[i]['year'],
+        classType: maps[i]['classType'],
+        classNum: maps[i]['classNum'],
+        grade: maps[i]['grade'],
+      );
+    });
+  }
+
   const Calc({super.key});
   static double gpa = 0.0;
   static double allgpa = 0.0;
@@ -403,6 +366,11 @@ class Calc extends StatelessWidget {
       child: Scaffold(
         floatingActionButton: FloatingActionButton.large(
             onPressed: () {
+              Year yearx = new Year(
+                  id: 1, year: 33, classType: 22, classNum: 22, grade: 4.4);
+              insertYear(yearx);
+              print(year());
+
               allgpa = gpaCalculatorAll(classGrade, classType);
               gpa = gpaCalculator(classGrade, classType);
 
